77a5c667e29112e45c8f3a60ff90c1731951cf25
mochammadsufandi <mochammaddwi09@gmail.com>
2024-02-25 20:59:49 +0700

    add variable for local stroage (EDIT_REGISTER), method editList

--- scriptlist.js
+++ scriptlist.js
@@ -1,8 +1,8 @@
 const LOCAL_STORAGE = 'list-register';
 const RENDER_EVENT  = 'render-list-register';
 const SAVED_EVENT = 'list-register';
+const EDIT_REGISTER = 'edit-register';
 const arrRegister = [];
-
 
 // create a class for saving data in local storage
 class storageSaver {
@@ -41,22 +41,64 @@ class storageSaver {
             <td>${obj.age}</td>
             <td>${obj.saku}</td>
             <td class="text-center">
-                <button type="button" class="btn btn-warning">Edit</button>
+                <a href="#"><button type="button" class="btn btn-warning" id="edit-btn">Edit</button></a>
                 <button type="button" class="btn btn-danger">Delete</button>
             </td>`
             tableContent.appendChild(rowElement);
         })
- 
+    }
+
+    editList () {
+        const trTable = document.querySelectorAll('#edit-btn');
+        let registerEdit = [];
+        
+        for (const index in arrRegister) {
+            trTable[index].addEventListener('click',()=> {
+                registerEdit.push(arrRegister[index]);
+                const parsed = JSON.stringify(registerEdit);
+                localStorage.setItem(this.name,parsed);
+                console.log(registerEdit);
+            })
+        }
+        registerEdit = [];
+        // document.dispatchEvent(new Event(RENDER_EVENT));
+        
+    }
+
+    editProcess () {
+       
+
+
     }
 }
 
+// class register {
+//     constructor (name,age,saku) {
+//         // this.id = +new Date(); 
+//         this.name = name;
+//         this.age = age;
+//         this.saku = saku;
+//     }
+
+    
+// }
 
 const storageLocal = new storageSaver(LOCAL_STORAGE);
+const editStorage = new storageSaver(EDIT_REGISTER);
+
 
 // For loading data 
 document.addEventListener('DOMContentLoaded', () => {
     document.dispatchEvent(new Event(RENDER_EVENT));
+    document.dispatchEvent(new Event(SAVED_EVENT));
+    
 })
+
+// For Saving Edit Data Register 
+document.addEventListener(SAVED_EVENT, ()=> {
+    editStorage.editList();
+}
+)
 // For Rendering Data
 document.addEventListener(RENDER_EVENT,() => {
     setTimeout(() => {
@@ -64,8 +106,13 @@ document.addEventListener(RENDER_EVENT,() => {
             storageLocal.loadDataLocal();
             storageLocal.showData();
         }
-        
+      
+        // storageLocal.editList();
+        editStorage.editList();
     },2000);
+    
     
     // arrRegister = [];
 })
+
+// For Editing List Register
