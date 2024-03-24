ad0ebb5ecb93c8a892349f5ea7b073c6baffc789
mochammadsufandi <mochammaddwi09@gmail.com>
2024-03-24 15:51:11 +0700

    add method for resume

--- scriptlist.js
+++ scriptlist.js
@@ -3,6 +3,7 @@ const RENDER_EVENT  = 'render-list-register';
 const SAVED_EVENT = 'list-register';
 const EDIT_REGISTER = 'edit-register';
 const arrRegister = [];
+const arrDelete = [];
 
 // create a class for saving data in local storage
 class storageSaver {
@@ -42,7 +43,7 @@ class storageSaver {
             <td>${obj.saku}</td>
             <td class="text-center">
                 <a href="index.html"><button type="button" class="btn btn-warning" id="edit-btn">Edit</button></a>
-                <button type="button" class="btn btn-danger" id="delete-btn">Delete</button>
+                <button type="button" class="btn btn-danger" id="delete-btn" data-bs-toggle="modal" data-bs-target="#DeleteModal">Delete</button>
             </td>`
             tableContent.appendChild(rowElement);
         })
@@ -68,37 +69,92 @@ class storageSaver {
 
         for (const index in arrRegister) {
             deleteBtn[index].addEventListener('click', () => {
-                window.alert('oiioioi');
+                const deleteItem = arrRegister[index];
+                arrDelete[0] = deleteItem;
+                console.log(deleteItem);
             })
         }
     }
+
+    resume () {
+        let sumSaku = 0;
+        let sumAge = 0;
+        arrRegister.forEach((obj) => {
+            sumSaku += Number(obj.saku);
+            sumAge += Number(obj.age);
+        })
+        const sakuMean = Math.round(sumSaku/arrRegister.length);
+        const ageMean =  Math.round(sumAge/arrRegister.length);
+
+        const tableResume = document.getElementById('table-resume');
+        tableResume.innerHTML = '';
+        const rowElement = document.createElement('tr');
+        rowElement.innerHTML = `
+            <td class="text-center">${sakuMean}</td>
+            <td class="text-center">${ageMean}</td>`;
+        tableResume.appendChild(rowElement);
+       
+    }
 }
 
-// class register {
-//     constructor (name,age,saku) {
-//         // this.id = +new Date(); 
-//         this.name = name;
-//         this.age = age;
-//         this.saku = saku;
-//     }
+class register {
+    constructor(id,name,age,saku) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.saku = saku;
+    }
+}
 
-    
-// }
+class deleteRegister extends register {
+    // method
+    findIndex () {
+        for (let index in arrRegister) {
+            if(arrDelete[0].id === arrRegister[index].id) {
+                return index;
+            }
+        }
+    }
+    deleteReg (index) {
+        arrRegister.splice(index,1);   
+    }
+}
 
+// variable for storage
 const storageLocal = new storageSaver(LOCAL_STORAGE);
 const editStorage = new storageSaver(EDIT_REGISTER);
+
+// variable for editing
+// const dialogBoxBtn = document.getElementById()
 
 
 // For loading data 
 document.addEventListener('DOMContentLoaded', () => {
-    document.dispatchEvent(new Event(RENDER_EVENT));
-    document.dispatchEvent(new Event(SAVED_EVENT));
+    const delBtn = document.getElementById('delete-button');
+
+    delBtn.addEventListener('click', () => {
+        const del = new deleteRegister(
+            arrDelete[0].id,
+            arrDelete[0].name,
+            arrDelete[0].age,
+            arrDelete[0].saku
+        )
+        const index = del.findIndex();
+        del.deleteReg(index);
+        document.dispatchEvent(new Event(SAVED_EVENT));
+        location.reload();
+    })
     
+    document.dispatchEvent(new Event(RENDER_EVENT));
 })
+
+    
 
 // For Saving Edit Data Register 
 document.addEventListener(SAVED_EVENT, ()=> {
-    editStorage.editList();
+    storageLocal.saveDataLocal();
+    // editStorage.editList();
+
 }
 )
 // For Rendering Data
@@ -107,15 +163,13 @@ document.addEventListener(RENDER_EVENT,() => {
         if (storageLocal) {
             storageLocal.loadDataLocal();
             storageLocal.showData();
+            storageLocal.resume();
         }
       
         // storageLocal.editList();
         editStorage.editList();
         editStorage.deleteList();
     },2000);
-    
-    
-    // arrRegister = [];
 })
 
-// For Editing List Register
+
